c9f6f3f5bb69dd6838e41d1664c3cacd6a7a95bd
github username <67525690+kirstybrews@users.noreply.github.com>
2021-05-07 18:26:02 -0500

    added 5 nominations banner

--- src/components/Nominations.js
+++ src/components/Nominations.js
@@ -5,6 +5,7 @@ const Nominations = ({nominations, setNominations}) => {
     return (
         <div>
             <h3>Nominations</h3>
+            {nominations.length === 5 ? <div>You have 5 nominations!</div> : null}
             <ul>
                 {nominations.map(nomination => <NominatedMovie key={nomination.imdbID} movie={nomination} setNominations={setNominations} nominations={nominations}/>)}
             </ul>
