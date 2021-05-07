ebc60ec577081f96f898aac8aa7e339b47c129c5
github username <67525690+kirstybrews@users.noreply.github.com>
2021-05-07 18:06:43 -0500

    disable nominate button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Movie = ({movie, nominations, setNominations}) => {
+    const [disabled, setDisabled] = useState(false)
 
     const clickHandler = () => {
         if (nominations.length < 5) {
             setNominations([...nominations, movie])
+            setDisabled(true)
         }
     }
     return (
         <li>
             <span>{movie.Title} ({movie.Year})</span>
-            <button onClick={clickHandler} disabled={null}>Nominate</button>
+            <button onClick={clickHandler} disabled={disabled}>Nominate</button>
         </li>
     )
 }
