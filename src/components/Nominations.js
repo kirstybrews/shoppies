0ea52da7456cd39c6444c2e4887ab1b1ceaa9c8d
github username <67525690+kirstybrews@users.noreply.github.com>
2021-05-15 21:53:43 -0500

    changed banner for faster loading

--- src/components/Nominations.js
+++ src/components/Nominations.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import NominatedMovie from './NominatedMovie';
-import FiveNoms from './FiveNoms.png';
 
 const Nominations = ({nominations, setNominations}) => {
     return (
         <section className="w3-light-grey flex-item w3-mobile">
             <h3>Nominations</h3>
-            {nominations.length === 5 ? <img src={FiveNoms} alt="You have 5 nominations!"/> : null}
+            {nominations.length === 5 ? <div id="five-noms"><strong>You have 5 nominations!</strong></div> : null}
             <ul>
                 {nominations.map(nomination => <NominatedMovie key={nomination.imdbID} movie={nomination} setNominations={setNominations} nominations={nominations}/>)}
             </ul>
